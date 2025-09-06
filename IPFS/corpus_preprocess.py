c397150ad0724d99816dfe9ee43cf749a36c7254
Mark Bailey <32108703+ma-da@users.noreply.github.com>
2025-09-05 21:20:14 -0500

    Update corpus_preprocess.py

--- IPFS/corpus_preprocess.py
+++ IPFS/corpus_preprocess.py
@@ -7,11 +7,13 @@ Should preprocess data for LLM training and IPFS storage, plus complete indexing
 2. Chunk text into chunks - maybe 480 tokens max with an overlap of 40 and smaller chunks okay if chunk is end of page/file?
 - Add Source and Question info (plus stop token?) to each chunk
 - Create LLM input file from annotated chunks
-- Generate TF-IDF and BGE vectors for corpus
+- Generate TF-IDF vectors for corpus
 - Compute vector space centroids for clustering and search routing
 - Turn each chunk into a json file inc at minimum Source, Question, and text
-3. Store each chunk on IPFS, using returned cids + vectors to build indexes and index manifests for TF-IDF and BGE, inc primary index plus one index per centroid
-- Store TF-IDF vocabulary + manifest info for recomputing values as dataset grows
+3. Store each chunk on IPFS, using returned cids + vectors to build indexes and index manifests, inc primary index plus one index per centroid
+- Store TF-IDF vocabulary/IDF + manifest info for recomputing values as dataset grows
 - Register index manifests on Hive blockchain
-- Generate master index of source/question/cid that isn't publicly exposed    
+- Generate master index of source/question/cid that isn't publicly exposed
+- Compute BGE embeddings on the fly for closest n TF-IDF search result values 
+
 '''
