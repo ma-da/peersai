5c7582ff06ca55418417134a454a4856ccd6f3bd
Mark Bailey <32108703+ma-da@users.noreply.github.com>
2025-09-18 11:19:23 -0500

    Update do_corpus_indexing.py

--- preprocessing/do_corpus_indexing.py
+++ preprocessing/do_corpus_indexing.py
@@ -11,7 +11,7 @@ def main():
 
     groups, centroids = calculate_kmeans.calculate_kmeans(X)
 
-    # Mark – Is passing combined df right?
+    # Mark – Is passing combined df right? N - it looks right if combined_df at this stage is the same as what I was passing to k means in the original script
     calculate_kmeans.build_index_shards(X, vectorizer, groups, centroids, combined_df)
 
     corpus_indexing.do_local_tf_idf_search_test()
