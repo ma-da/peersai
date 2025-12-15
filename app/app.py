22835379eec6208c248d5f43c91881c6a79c4c2c
NateD1105 <nate_d1105>
2025-12-15 16:45:13 -0500

    add api hooks

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, jsonify, request
 
 app = Flask(__name__)
 
@@ -32,6 +32,62 @@ def index():
 </body>
 </html>
 """)
+
+@app.route("/api/chat", methods=["POST","GET"])
+def on_chat():
+    #payload = request.get_json(silent=True)
+    #if payload is None:
+    #    return jsonify({
+    #        "ok": False,
+    #        "error": "Invalid or missing JSON body"
+    #    }), 400
+
+    return jsonify({
+        "ok": True,
+        "status": "chat was successful",
+    }), 200
+
+@app.route("/api/search", methods=["POST","GET"])
+def on_search():
+    #payload = request.get_json(silent=True)
+    #if payload is None:
+    #    return jsonify({
+    #        "ok": False,
+    #        "error": "Invalid or missing JSON body"
+    #    }), 400
+
+    return jsonify({
+        "ok": True,
+        "status": "search was successful",
+    }), 200
+
+@app.route("/api/feedback", methods=["POST","GET"])
+def on_feedback():
+    #payload = request.get_json(silent=True)
+    #if payload is None:
+    #    return jsonify({
+    #        "ok": False,
+    #        "error": "Invalid or missing JSON body"
+    #    }), 400
+
+    return jsonify({
+        "ok": True,
+        "status": "feedback was successful",
+    }), 200
+
+@app.route("/api/status", methods=["POST","GET"])
+def on_status():
+    #payload = request.get_json(silent=True)
+    #if payload is None:
+    #    return jsonify({
+    #        "ok": False,
+    #        "error": "Invalid or missing JSON body"
+    #    }), 400
+
+    return jsonify({
+        "ok": True,
+        "status": "status was successful",
+    }), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
